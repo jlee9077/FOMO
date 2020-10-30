42f210b9bfeffae14dd69933e6174f8e3c4a1ca5
Joseph Lee <jlee9077@gmail.com>
2020-10-30 03:58:22 -0400

    Implemented Navigation and Background

--- crypto/App.js
+++ crypto/App.js
@@ -1,80 +1,28 @@
-import React, { Component } from 'react';
-import { AppLoading } from 'expo';
-import { Platform, StatusBar, StyleSheet } from 'react-native';
-import { Root, View, Container, Content, Text, Spinner } from 'native-base'
-import TitleBar from './components/TitleBar'
-import CryptoCoin from './components/CryptoCoin'
-import * as Font from 'expo-font';
+import React, { Component } from 'react'
+import 'react-native-gesture-handler'
+import Home from './components/Home'
+import CoinList from './components/CoinList'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
 
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isLoading: true,
-    };
-  }
-
-  async componentDidMount() {
-    await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
-    });
-    this.setState({ isLoading: false });
-  }
   render() {
-    const { isLoading } = this.state
-
-    if (isLoading) {
-      return <AppLoading />
-    }
     return (
-      <Root>
-        <View style={styles.container}>
-          <Container style={{ backgroundColor: '#000000' }}>
-            <TitleBar title="Cryptocurrencies"/>
-            <Content>
-              <CryptoCoin
-               name='BTC'
-               quote='USDT'
-               time='30m'
-              />
-              <CryptoCoin
-               name='ETH'
-               quote='USDT'
-               time='30m'
-              />
-              <CryptoCoin
-               name='XRP'
-               quote='USDT'
-               time='30m'
-              />
-              <CryptoCoin
-               name='BCH'
-               quote='USDT'
-               time='30m'
-              />
-              <CryptoCoin
-               name='LINK'
-               quote='USDT'
-               time='30m'
-              />
-            </Content>
-          </Container>
-        </View>
-      </Root>
-    );
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            barStyle={{backgroundColor: '#000000'}}
+          />
+          <Stack.Screen
+            name="Cryptos"
+            component={CoinList}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000',
-    alignSelf: 'stretch'
-    // ...Platform.select({
-    //   android: {
-    //     marginTop: StatusBar.currentHeight
-    //   }
-    // })
-  }
-});
---
+++ crypto/components/CoinList.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { AppLoading } from 'expo';
+import { Platform, StatusBar, StyleSheet } from 'react-native';
+import { Root, View, Container, Content, Text, Spinner, Button } from 'native-base'
+import TitleBar from '../components/TitleBar'
+import CryptoCoin from '../components/CryptoCoin'
+import * as Font from 'expo-font';
+
+export default class CoinList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf')
+    });
+    this.setState({ isLoading: false });
+  }
+  render() {
+    const { isLoading } = this.state
+
+    if (isLoading) {
+      return <AppLoading />
+    }
+    return (
+      <Root>
+        <View style={styles.container}>
+          <Container style={{ backgroundColor: '#000000' }}>
+            <TitleBar title="Cryptocurrencies"/>
+            <Content>
+              <CryptoCoin
+               name='BTC'
+               quote='USDT'
+               time='30m'
+              />
+              <CryptoCoin
+               name='ETH'
+               quote='USDT'
+               time='30m'
+              />
+              <CryptoCoin
+               name='XRP'
+               quote='USDT'
+               time='30m'
+              />
+              <CryptoCoin
+               name='BCH'
+               quote='USDT'
+               time='30m'
+              />
+              <CryptoCoin
+               name='LINK'
+               quote='USDT'
+               time='30m'
+              />
+            </Content>
+          </Container>
+        </View>
+      </Root>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignSelf: 'stretch'
+  }
+});
